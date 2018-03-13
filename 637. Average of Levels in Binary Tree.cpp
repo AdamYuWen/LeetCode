d5ba8cbe264011899d25bb4e94fba354d874a549
Yu Wen <wen.adam@hotmail.com>
2018-03-13 10:01:34 -0400

    Add files via upload

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -1,32 +1,34 @@
 /**
- * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
- * Example 1:
- * Input:
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- * Output: [3, 14.5, 11]
- * Explanation:
- * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
- * Note:
- * The range of node's value is in the range of 32-bit signed integer.
- * https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
- * 
- * Author: Adam Yu Wen
- * Date: March 12, 2018
- */
+* Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+* Example 1:
+* Input:
+*     3
+*    / \
+*   9  20
+*     /  \
+*    15   7
+* Output: [3, 14.5, 11]
+* Explanation:
+* The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+* Note:
+* The range of node's value is in the range of 32-bit signed integer.
+* https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+*
+* Author: Adam Yu Wen
+* Date: March 12, 2018
+*/
 
- /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+// Method 1:
 class Solution {
 public:
 	vector<double> averageOfLevels(TreeNode* root) {
@@ -50,5 +52,36 @@ public:
 			res.push_back(sum / double(qSize));
 		}
 		return res;
+	}
+};
+
+
+// Method 2:
+class Solution {
+public:
+	vector<double> averageOfLevels(TreeNode* root) {
+		vector<double> res;
+		vector<long> levelSum;
+		vector<int> count;
+		levelAndCount(root, 0, levelSum, count);
+		int levelSize = levelSum.size();
+		for (int idx = 0; idx < levelSize; idx++) {
+			res.push_back(levelSum[idx] / double(count[idx]));
+		}
+		return res;
+	}
+
+	void levelAndCount(TreeNode* root, int level, vector<long> &levelSum, vector<int> &count) {
+		if (root == NULL) return;
+		if (level >= levelSum.size()) {
+			levelSum.push_back(root->val);
+			count.push_back(1);
+		}
+		else {
+			levelSum[level] += root->val;
+			count[level]++;
+		}
+		levelAndCount(root->left, level + 1, levelSum, count);
+		levelAndCount(root->right, level + 1, levelSum, count);
 	}
 };
