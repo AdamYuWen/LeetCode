d2582b6b4ed1bc9355f77e2e0d1f6818f33be025
Yu Wen <wen.adam@hotmail.com>
2018-03-14 11:56:22 -0400

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -32,14 +32,14 @@
  * Date: March 14, 2018
  */
 
- /**
-  * Definition for binary tree with next pointer.
-  * struct TreeLinkNode {
-  *  int val;
-  *  TreeLinkNode *left, *right, *next;
-  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-  * };
-  */
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	void connect(TreeLinkNode *root) {
