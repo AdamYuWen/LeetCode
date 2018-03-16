63cb0bd3f3e620add62301bf5df84f0e834e8cf0
Yu Wen <wen.adam@hotmail.com>
2018-03-16 08:25:25 -0400

    Self Solution

--- 230. Kth Smallest Element in a BST.cpp
+++ 230. Kth Smallest Element in a BST.cpp
@@ -40,3 +40,35 @@ public:
 		return res;
 	}
 };
+
+
+// Method 2:
+// Its runtime is 15ms.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	int kthSmallest(TreeNode* root, int k) {
+		int cnt = 0;
+		stack<TreeNode*> s;
+		TreeNode* p = root;
+		while (p || !s.empty()) {
+			while (p) {
+				s.push(p);
+				p = p->left;
+			}
+			p = s.top();
+			s.pop();
+			if (++cnt == k) return p->val;
+			p = p->right;
+		}
+		return -1;
+	}
+};
