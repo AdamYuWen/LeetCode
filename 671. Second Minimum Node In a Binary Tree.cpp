df9c316a8f7b41859765af06704b7f3e7a1310d2
Yu Wen <wen.adam@hotmail.com>
2018-03-17 16:01:16 -0400

    Self Solution

---
+++ 671. Second Minimum Node In a Binary Tree.cpp
@@ -0,0 +1,69 @@
+/**
+ * Given a non-empty special binary tree consisting of nodes with the non-negative
+ * value, where each node in this tree has exactly two or zero sub-node. If the node
+ * has two sub-nodes, then this node's value is the smaller value among its two sub-nodes.
+ * Given such a binary tree, you need to output the second minimum value in the set
+ * made of all the nodes' value in the whole tree.
+ *
+ * If no such second minimum value exists, output -1 instead.
+ * Example 1:
+ *	Input:
+ *	    2
+ *	   / \
+ *	  2   5
+ *	     / \
+ *	    5   7
+ *	Output: 5
+ *	Explanation: The smallest value is 2, the second smallest value is 5.
+ * Example 2:
+ *	Input:
+ *	    2
+ *	   / \
+ *	  2   2
+ *	Output: -1
+ *	Explanation: The smallest value is 2, but there isn't any second smallest value.
+ * https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/description/
+ *
+ * Author: Adam Yu Wen
+ * Date: March 17, 2018
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> res;
+	int findSecondMinimumValue(TreeNode* root) {
+		inorderTraversal(root);
+		int smallest = INT_MAX, secondSmallest = INT_MAX;
+		int resSize = res.size();
+		for (int idx = 0; idx < resSize; idx++) {
+			if (res[idx] == smallest) {
+				continue;
+			}
+			if (res[idx] < smallest) {
+				secondSmallest = smallest;
+				smallest = res[idx];
+			}
+			else if (res[idx] < secondSmallest) {
+				secondSmallest = res[idx];
+			}
+		}
+		return secondSmallest == INT_MAX ? -1 : secondSmallest;
+	}
+
+	vector<int> inorderTraversal(TreeNode* root) {
+		if (!root) return res;
+		inorderTraversal(root->left);
+		res.push_back(root->val);
+		inorderTraversal(root->right);
+		return res;
+	}
+};
