80c43a0a420e5d680e846c918c109a3cfd4ec7c6
Yu Wen <wen.adam@hotmail.com>
2018-03-20 10:13:05 -0400

    Update 572. Subtree of Another Tree.cpp

--- 572. Subtree of Another Tree.cpp
+++ 572. Subtree of Another Tree.cpp
@@ -35,6 +35,7 @@
  * Date: March 20, 2018
  */
 
+// Method 1:
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -67,4 +68,32 @@ public:
 			inorderTraversal(t->right, res);
 		}
 	}
+};
+
+// Method 2:
+// Reference: http://www.cnblogs.com/grandyang/p/6828687.html
+// It takes 26 ms, which means it is faster than the first method.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if (!s) return false;
+        if (isSameTree(s,t)) return true;
+        return isSubtree(s->left, t) || isSubtree(s->right,t);
+    }
+    
+    bool isSameTree(TreeNode* s, TreeNode* t) {
+        if (!s && !t) return true;
+        if (!s || !t) return false;
+        if (s->val != t->val) return false;
+        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+    }
 };
