a6d8c63e09aba43ab7af0630e2f1bfb92889aa1e
Yu Wen <wen.adam@hotmail.com>
2018-03-20 10:14:40 -0400

    Self Solution

--- 572. Subtree of Another Tree.cpp
+++ 572. Subtree of Another Tree.cpp
@@ -1,50 +1,50 @@
 /**
- * Given two non-empty binary trees s and t, check whether tree t has exactly the
- * same structure and node values with a subtree of s. A subtree of s is a tree
- * consists of a node in s and all of this node's descendants. The tree s could
- * also be considered as a subtree of itself.
- * Example 1:
- * Given tree s:
- *	     3
- *	    / \
- *	   4   5
- *	  / \
- *	 1   2
- *	Given tree t:
- *	   4
- *	  / \
- *	 1   2
- *	Return true, because t has the same structure and node values with a subtree of s.
- * Example 2:
- *	Given tree s:
- *	     3
- *	    / \
- *	   4   5
- *	  / \
- *	 1   2
- *	    /
- *	   0
- *	Given tree t:
- *	   4
- *	  / \
- *	 1   2
- *	Return false.
- * https://leetcode.com/problems/subtree-of-another-tree/description/
- *
- * Author: Adam Yu Wen
- * Date: March 20, 2018
- */
+* Given two non-empty binary trees s and t, check whether tree t has exactly the
+* same structure and node values with a subtree of s. A subtree of s is a tree
+* consists of a node in s and all of this node's descendants. The tree s could
+* also be considered as a subtree of itself.
+* Example 1:
+* Given tree s:
+*	     3
+*	    / \
+*	   4   5
+*	  / \
+*	 1   2
+*	Given tree t:
+*	   4
+*	  / \
+*	 1   2
+*	Return true, because t has the same structure and node values with a subtree of s.
+* Example 2:
+*	Given tree s:
+*	     3
+*	    / \
+*	   4   5
+*	  / \
+*	 1   2
+*	    /
+*	   0
+*	Given tree t:
+*	   4
+*	  / \
+*	 1   2
+*	Return false.
+* https://leetcode.com/problems/subtree-of-another-tree/description/
+*
+* Author: Adam Yu Wen
+* Date: March 20, 2018
+*/
 
 // Method 1:
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
 class Solution {
 public:
 	bool isSubtree(TreeNode* s, TreeNode* t) {
@@ -74,26 +74,26 @@ public:
 // Reference: http://www.cnblogs.com/grandyang/p/6828687.html
 // It takes 26 ms, which means it is faster than the first method.
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
 class Solution {
 public:
-    bool isSubtree(TreeNode* s, TreeNode* t) {
-        if (!s) return false;
-        if (isSameTree(s,t)) return true;
-        return isSubtree(s->left, t) || isSubtree(s->right,t);
-    }
-    
-    bool isSameTree(TreeNode* s, TreeNode* t) {
-        if (!s && !t) return true;
-        if (!s || !t) return false;
-        if (s->val != t->val) return false;
-        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
-    }
+	bool isSubtree(TreeNode* s, TreeNode* t) {
+		if (!s) return false;
+		if (isSameTree(s, t)) return true;
+		return isSubtree(s->left, t) || isSubtree(s->right, t);
+	}
+
+	bool isSameTree(TreeNode* s, TreeNode* t) {
+		if (!s && !t) return true;
+		if (!s || !t) return false;
+		if (s->val != t->val) return false;
+		return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+	}
 };
