b9be516215655038e22d43d76e2bf9e6ac5bd7b7
Yu Wen <wen.adam@hotmail.com>
2018-04-18 17:25:06 -0400

    Added a faster solution.

--- 096. Unique Binary Search Trees.cpp
+++ 096. Unique Binary Search Trees.cpp
@@ -4,9 +4,9 @@
  * For example,
  * Given n = 3, there are a total of 5 unique BST's.
  *   1         3     3      2      1
- *    \       /     /      / \                  \
+ *    \       /     /      / \      \
  *     3     2     1      1   3      2
- *    /     /       \                           \
+ *    /     /       \                 \
  *   2     1         2                 3
  * https://leetcode.com/problems/unique-binary-search-trees/description/
  *
@@ -14,6 +14,8 @@
  * Date: April 6, 2018
  */
 
+// Method 1:
+// It is slow, which takes 108 ms.
 class Solution {
 public:
     int numTrees(int n) {
@@ -29,5 +31,27 @@ public:
             res += numTrees(idx - 1) * numTrees(n - idx);
         }
         return res;
+    }
+};
+
+// Method 2:
+// It takes 2 ms, which beats 100% of cpp submissions.
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> resList;
+        for (int i = 0; i < n + 1; i++) {
+            if (i == 0 || i == 1) {
+                resList.push_back(1);
+            }
+            else {
+                int res = 0;
+                for (int j = 0; j < i; j++) {
+                    res += resList[j] * resList[i - 1 - j];
+                }
+                resList.push_back(res);
+            }
+        }
+        return resList.back();
     }
 };
