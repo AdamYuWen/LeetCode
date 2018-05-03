fe80552ada3b61883db851a9366275109d210238
Wen <wen.adam@hotmail.com>
2018-05-03 14:31:22 -0400

    Self Solution

---
+++ 560. Subarray Sum Equals K.cpp
@@ -0,0 +1,30 @@
+/**
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+ * Example 1:
+ * Input:nums = [1,1,1], k = 2
+ * Output: 2
+ * Note:
+ * The length of the array is in range [1, 20,000].
+ * The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+ * https://leetcode.com/problems/subarray-sum-equals-k/description/
+ *
+ * Author: Adam Yu Wen
+ * Date: May 3, 2018
+ */
+ 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int res = 0, sum = 0;
+        int numsSize = nums.size();
+        for (int i = 0; i < numsSize; i++) {
+            if (nums[i] == k) res++;
+            sum = nums[i];
+            for (int j = i + 1; j < numsSize; j++) {
+                sum += nums[j];
+                if (sum == k) res++;
+            }
+        }
+        return res;
+    }
+};
