ddf71585f2e710ebcb54e4246ac354b93b9b8ce1
Wen <wen.adam@hotmail.com>
2018-05-07 17:16:01 -0400

    Self Solution

---
+++ 830. Positions of Large Groups.cpp
@@ -0,0 +1,43 @@
+/* In a string S of lowercase letters, these letters form consecutive groups of the same character.
+ * For example, a string like S = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z" and "yy".
+ * Call a group large if it has 3 or more characters.  We would like the starting and ending positions of every large group.
+ * The final answer should be in lexicographic order.
+ * Example 1:
+ * Input: "abbxxxxzzy"
+ * Output: [[3,6]]
+ * Explanation: "xxxx" is the single large group with starting  3 and ending positions 6.
+ * Example 2:
+ * Input: "abc"
+ * Output: []
+ * Explanation: We have "a","b" and "c" but no large group.
+ * Example 3:
+ * Input: "abcdddeeeeaabbbcd"
+ * Output: [[3,5],[6,9],[12,14]]
+ * Note:  1 <= S.length <= 1000
+ * https://leetcode.com/problems/positions-of-large-groups/description/
+ *
+ * Author: Adam Yu Wen
+ * Date: May 7, 2018
+ */
+
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string S) {
+        vector<vector<int>> res;
+        
+        int l = 0, r = 0;
+        int SSize = S.size();
+        while (l < SSize) {
+            while (r < SSize && S[l] == S[r]) r++;
+            if (r - l > 2) {
+                vector<int> resTmp(2);
+                resTmp[0] = l;
+                resTmp[1] = r - 1;
+                res.push_back(resTmp);
+            }
+            l = r;
+        }
+        
+        return res;
+    }
+};
