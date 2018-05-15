3fa043173d621de81e9a346fd26e4ba51a37435c
Wen <wen.adam@hotmail.com>
2018-05-14 22:15:51 -0400

    Self Solution

--- Python/049. Group Anagrams.py
+++ Python/049. Group Anagrams.py
@@ -1,21 +1,21 @@
- # Given an array of strings, group anagrams together.
- # Example:
- # Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
- # Output:
- # [
- #   ["ate","eat","tea"],
- #   ["nat","tan"],
- #   ["bat"]
- # ]
- # Note:
- # All inputs will be in lowercase.
- # The order of your output does not matter.
- # https://leetcode.com/problems/group-anagrams/description/
- # 
- # Author: Adam Yu Wen
- # Date: May 14, 2018
+# Given an array of strings, group anagrams together.
+# Example:
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+# Note:
+# All inputs will be in lowercase.
+# The order of your output does not matter.
+# https://leetcode.com/problems/group-anagrams/description/
+# 
+# Author: Adam Yu Wen
+# Date: May 14, 2018
 
- class Solution(object):
+class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
