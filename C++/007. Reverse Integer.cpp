b13d997fe02cdcd2a90e852aab5249221040453e
Wen <wen.adam@hotmail.com>
2018-06-21 14:09:31 -0400

    Self Solution

--- C++/007. Reverse Integer.cpp
+++ C++/007. Reverse Integer.cpp
@@ -10,26 +10,26 @@
 
 class Solution {
 public:
-	int reverse(long long x) {
-		// the type of parameter is changed to long long
-		// if x is larger the scope of int, return 0
-		// for example, if x is 2147483678, it returns 0
-		if (x > INT_MAX) return 0;
-		if (x >= 0) {
-			if (x < 10) return x;
-			else {
-				long long res = 0;
-				while (x >= 10) {
-					res = res * 10 + (x % 10);
-					x /= 10;
-				}
-				res = res * 10 + x;
-				if (res > INT_MAX) return 0;
-				else return int(res);
-			}
-		}
-		else {
-			return -reverse(-x);
-		}
-	}
+    int reverse(long long x) {
+        // the type of parameter is changed to long long
+        // if x is larger the scope of int, return 0
+        // for example, if x is 2147483678, it returns 0
+        if (x > INT_MAX) return 0;
+        if (x >= 0) {
+            if (x < 10) return x;
+            else {
+                long long res = 0;
+                while (x >= 10) {
+                    res = res * 10 + (x % 10);
+                    x /= 10;
+                }
+                res = res * 10 + x;
+                if (res > INT_MAX) return 0;
+                else return int(res);
+            }
+        }
+        else {
+            return -reverse(-x);
+        }
+    }
 };
