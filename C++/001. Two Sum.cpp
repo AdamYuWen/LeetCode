7f88e9c1281b01acd8243d5c6f5169e66545702b
Wen <wen.adam@hotmail.com>
2018-06-21 13:57:30 -0400

    Self Solution

--- C++/001. Two Sum.cpp
+++ C++/001. Two Sum.cpp
@@ -15,20 +15,20 @@
 
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		unordered_map<int, int> myMap;
-		vector<int> res;
-		for (int i = 0; i < nums.size(); i++) {
-			myMap[nums[i]] = i;
-		}
-		for (int i = 0; i < nums.size(); i++) {
-			int diff = target - nums[i];
-			if (myMap.count(diff) && myMap[diff] != i) {
-				res.push_back(i);
-				res.push_back(myMap[diff]);
-				break;
-			}
-		}
-		return res;
-	}
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> myMap;
+    vector<int> res;
+    for (int i = 0; i < nums.size(); i++) {
+      myMap[nums[i]] = i;
+    }
+    for (int i = 0; i < nums.size(); i++) {
+      int diff = target - nums[i];
+      if (myMap.count(diff) && myMap[diff] != i) {
+        res.push_back(i);
+        res.push_back(myMap[diff]);
+        break;
+      }
+    }
+    return res;
+  }
 };
